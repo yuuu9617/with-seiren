8da351766382d8aa98f65e7045f5a0b41472de5c
kita_yu <warno9617@gmail.com>
2024-11-07 20:35:58 +0900

    遷移テストついか

--- e2e/top.spec.ts
+++ e2e/top.spec.ts
@@ -4,3 +4,10 @@ test('user can go to the top page', async ({ page }) => {
   await page.goto('/');
   await expect(page.getByText('ダイエット成功のための契約を結ぶ')).toBeVisible();
 });
+
+test('user can navigate to weight loss commitment page', async ({ page }) => {
+  await page.goto('/');
+  const button = page.getByText('ダイエット成功のための契約を結ぶ');
+  await button.click();
+  await expect(page).toHaveURL('/weight-loss-commitment/create');
+});
